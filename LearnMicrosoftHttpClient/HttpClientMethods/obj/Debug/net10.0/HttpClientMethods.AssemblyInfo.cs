f715cf2fad56dec188bc83ffaadca4efce2b062a
massielcr <massiel.dev.ca@gmail.com>
2026-05-03 20:35:16 -0400

    LearnMicrosoftHttpClient

--- LearnMicrosoftHttpClient/HttpClientMethods/obj/Debug/net10.0/HttpClientMethods.AssemblyInfo.cs
+++ LearnMicrosoftHttpClient/HttpClientMethods/obj/Debug/net10.0/HttpClientMethods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HttpClientMethods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53dfdb399fb027529aabf9d1af5541d3589b58f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ddbbb82c9f35edbcad27ff1a0671ef72c40ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HttpClientMethods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HttpClientMethods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
