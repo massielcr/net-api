088cbd609799fd9e639fdc1b5e525492333161ed
massielcr <massiel.dev.ca@gmail.com>
2026-05-04 21:54:40 -0400

    WebAPIClient tutorial

---
+++ LearnMicrosoftHttpClient/WebAPIClient/Dtos/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebAPIClient.Dtos
+{
+    public record class Repository(
+        string Name, 
+        string Description, 
+        [property:JsonPropertyName("html_url")] Uri GitHubHomeUrl,
+        Uri Homepage,
+        int Watchers,
+        [property: JsonPropertyName("pushed_at")] DateTime LastPushUtc)
+    {
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+    };
+}
---
+++ LearnMicrosoftHttpClient/WebAPIClient/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using WebAPIClient.Dtos;
+
+using HttpClient httpClient = new();
+
+httpClient.DefaultRequestHeaders.Accept.Clear();
+httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+
+var repositories = await ProcessRepositoriesAsync(httpClient);
+
+foreach (var repo in repositories)
+{
+    Console.WriteLine($"Name: {repo.Name}");
+    Console.WriteLine($"Homepage: {repo.Homepage}");
+    Console.WriteLine($"GitHub: {repo.GitHubHomeUrl}");
+    Console.WriteLine($"Description: {repo.Description}");
+    Console.WriteLine($"Watchers: {repo.Watchers:#,0}");
+    Console.WriteLine($"Last push: {repo.LastPush}");
+    Console.WriteLine();
+}    
+
+
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient httpClient)
+{
+    var repositories = await httpClient.GetFromJsonAsync<List<Repository>>("https://api.github.com/orgs/dotnet/repos");
+
+    return repositories ?? [];    
+}
+
+
+
+
+
+
+Console.ReadLine();
